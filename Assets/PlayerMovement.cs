4644b7a70640d57ce05b10f339b426bfa54bfae0
Bryan H <bryan.huang1@education.nsw.gov.au>
2026-05-20 10:34:05 +1000

    WIP working on jumping + playershadow

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,12 +2,86 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
+public class PlayerJump : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f; 
     private Rigidbody2D rb;     
     private Vector2 moveInput; 
     private Animator animator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    [Header("Jumping")]
+    public float jumpForce = 5f;
+    public float jumpDuration = 0.4f;
+    private bool isJumping = false;
+    private float jumpTimer;
+
+     [Header("Visuals (Fake 3D)")]
+    public Transform shadowSprite; 
+    private Vector3 originalShadowScale;
+
+        void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (shadowSprite != null)
+        {
+            originalShadowScale = shadowSprite.localScale;
+        }
+    }
+
+    // Handles Input System "Move" action
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    // Handles Input System "Jump" action
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && !isJumping)
+        {
+            {
+            isJumping = true;
+            jumpTimer = jumpDuration; 
+            }
+            
+        
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+        rb.linearVelocity = moveInput * moveSpeed;
+
+       
+        if (isJumping)
+        {
+            if (jumpTimer > 0)
+            {
+                
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                jumpTimer -= Time.fixedDeltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        
+        UpdateShadow();
+    }
+
+    void UpdateShadow()
+    {
+        if (shadowSprite != null)
+        {
+        
+        }
+    }
+
+
+    
     void Start()    
     {
         rb = GetComponent<Rigidbody2D>();
